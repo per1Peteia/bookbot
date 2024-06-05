0a6871a8856f8d876722867e2fc659d227eba327
per1Peteia <justusmanthey@googlemail.com>
2024-06-05 13:27:30 +0200

    added print decoration; charcount.isalpha();

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
-   
+    print(f"--- Begin report of {book_path} ---")
+
     text = get_book_text(book_path)
     #print(text)
    
     word_count = get_word_count(text)
-    print (f"word count is {word_count}")
+    print (f"{word_count} words found in the document")
 
     char_count = get_char_count(text)
     print(char_count)
-
+    print("--- End report --- ")
 
 def get_book_text(path):
     with open(path) as f:
@@ -25,9 +26,12 @@ def get_char_count(text):
 
     for char in lower_text:
         if char in char_dict:
-            char_dict[char] += 1
+            if char.isalpha() == 1:
+                char_dict[char] += 1
         else:
-            char_dict[char] = 1
+                if char.isalpha() == 1:
+                    char_dict[char] = 1
     return char_dict
+
 
 main()
